3b05ee3b24499d8cd286d94db0f6b4441636ab95
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-12 14:50:30 +0600

    Add reducer files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from './App';
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
