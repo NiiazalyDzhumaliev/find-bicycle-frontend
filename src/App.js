97dc9821f8e209c0ac40ae2a6fd10d889ed547f1
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-12 16:52:04 +0600

    Add login part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Register from './components/Register';
+import Login from './components/Login';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/signup" component={Register} />
+      <Route path="/login" component={Login} />
     </Switch>
   </BrowserRouter>
 );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,2 +1,50 @@
-const Login = () => <div />;
+import axios from 'axios';
+import { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userLogin = {
+        email,
+        password,
+      };
+      const response = await axios.post(
+        'http://localhost:3000/auth/login',
+        userLogin,
+      );
+      console.log(response);
+    } catch (error) {
+      console.log('User is not registered');
+    }
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email:
+          <input
+            type="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
 export default Login;
