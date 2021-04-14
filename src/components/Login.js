f8c5f0a494bfffd74ebf7d72788f7042de5cf05f
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-14 15:26:04 +0600

    Redirect if signup and login are ok

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import _ from 'lodash';
 import loginAction from '../action/login.action';
 
 const Login = () => {
@@ -7,6 +9,7 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
   const token = useSelector((state) => state.login.token);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,8 +19,11 @@ const Login = () => {
     };
     dispatch(loginAction(userLogin));
   };
+  const emptyToken = _.isEmpty(token);
   localStorage.setItem('JWT', token);
-
+  if (!emptyToken) {
+    history.push('/');
+  }
   return (
     <div>
       <h1>Login</h1>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import _ from 'lodash';
 import signupAction from '../action/signup.action';
 
 const Register = () => {
@@ -8,6 +10,7 @@ const Register = () => {
   const [password, setPassword] = useState(null);
   const dispatch = useDispatch();
   const token = useSelector((state) => state.signup.token);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,7 +22,12 @@ const Register = () => {
 
     dispatch(signupAction(newUser));
   };
+
+  const emptyToken = _.isEmpty(token);
   localStorage.setItem('JWT', token);
+  if (!emptyToken) {
+    history.push('/');
+  }
   return (
     <div>
       <h1>Register</h1>
