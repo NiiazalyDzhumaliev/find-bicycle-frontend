654ffe58828182354aa75e216b41ca09064bbc17
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2021-04-15 15:10:35 +0600

    Redirect if not signed in

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,11 +1,17 @@
+import { useHistory } from 'react-router-dom';
+
 const Logout = () => {
+  const history = useHistory();
   const handleClick = () => {
     localStorage.removeItem('JWT');
+    history.push('/');
   };
   return (
-    <button type="button" onClick={handleClick}>
-      Logout
-    </button>
+    <div>
+      <button type="button" onClick={handleClick}>
+        Logout
+      </button>
+    </div>
   );
 };
 export default Logout;
